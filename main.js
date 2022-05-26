39927acd04704a39adcb21c8c46fdf9147dbf568
muhammadYus <74594824+muhammadYus@users.noreply.github.com>
2022-05-26 19:23:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+let x = 5;
+let sol = Math.sqrt(Math.pow(x, 2) - 5 * x + 1 );
+let a  = sol; 
+console.log(a);
+
+
+
+let y = 0;
+let solution = Math.sqrt(8 * y - Math.pow(x, 2) - 12 );
+let b = solution;
+console.log(b);
+
+
+
+
+
+
+let c  = Math.sqrt(4 - Math.sqrt(5 - 5));
+let d = c;
+let e = d;
+let f = e;
+console.log(c*d*e*f);
+
+
+let j = 1;
+let z = 90;
+let log = Math.log(Math.sin(z * Math.PI/180)) * 2 * Math.log(Math.pow(Math.sin(z * Math.PI/180), 2)) * j + 1
+log = 0;
+console.log(log);
